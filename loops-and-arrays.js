86814638a17ab13b7b0d8df17ae3743acc45a1e9
jensyfranco <jenmilafran@gmail.com>
2023-03-22 14:25:18 +0100

    feature huiswerk les 6 - loops

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -1,3 +1,4 @@
+
 // ==========================================
 // Opdracht 1
 // Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
@@ -12,8 +13,9 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
-
-
+for (let i = 0; i < names.length; i++ ) {
+    console.log(names[i] + "je");
+}
 
 // ==========================================
 // Opdracht 2
@@ -29,14 +31,19 @@ const numbers = [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
 
-
-
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        numbers[i] *= 2;
+    } else {
+        numbers[i] *= 3;
+    }
+} console.log(numbers);
 
 // ==========================================
 // Opdracht 3
-// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array het volume uitrekent.
-// Vervolgens moet de huidige waarde in de array overschreven worden met: "Het volume van [x] is [y]"
-// Het volume is lengte x breedte x hoogte
+// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array, het volume uitrekent.
+// Het volume van een vierkant is Lengte x Breedte x Hoogte
+// Vervolgens moet de huidige waarde in de array overschreven worden met: "Het volume van [a] is [b]"
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
 
@@ -46,10 +53,16 @@ const squares = [30, 2, 8, 24, 11];
 // Vóór het script zie je de originele waardes:
 // console.log(squares) geeft: [30, 2, 8, 24, 11];
 // Na jouw script zie je de aangepaste waardes:
-// console.log(squares) geeft: [
+// console.log(squares) geeft:
+// [
 //   'Het volume van 30 is 27000',
 //   'Het volume van 2 is 8',
 //   'Het volume van 8 is 512',
 //   'Het volume van 24 is 13824',
 //   'Het volume van 11 is 1331'
 // ]
+
+for (let i = 0; i < squares.length; i++) {
+    const volume = squares[i] * squares[i] * squares[i];
+    console.log("Het volume van " + squares[i] + " is " + volume);
+}
